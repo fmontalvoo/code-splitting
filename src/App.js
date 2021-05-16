0083392810f8d52d35d3fdc4c4fb0b0b9e4b999b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-16 16:39:46 -0500

    Cargando componentes bajo demanda.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, Suspense } from 'react';
 import './App.css';
 
+// Import dinamico que retorna una promesa con el componente solicitado.
+const Componente = React.lazy(() => import('./Componente'));
+
+// Obteniendo componentes bajo demanda.
+
 function App() {
+
+  const [showComponent, setState] = useState(false)
+
+  const handleShowComponent = (evt) => setState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* El componente `Componente` se descargara y mostrara unicamente al presionar
+          el boton mostrar. Por lo tanto el archivo javascript del que depende el componente
+          no sera descargado en el navegador hasta que este sea solicitados.
+      */}
+      <button onClick={handleShowComponent}>Mostrar componente</button>
+      <br />
+      <br />
+      {
+        showComponent
+        &&
+        /*
+         Suspense se encarga de mostrar nuestro componente cuando este
+         se termina de descargar. El prop fallback mostrara un componente
+         mientras el otro componente se descarga.
+        */
+        <Suspense fallback={<p>Cargando...</p>}><Componente /></Suspense>
+      }
     </div>
   );
 }
---
+++ src/Componente.js
@@ -0,0 +1,14 @@
+
+/**
+ *  Este componente no se cargara con los demas componentes de la aplicación,
+ *  si no, se descargara hasta que el usuario lo necesite.
+ */
+function Componente() {
+    return (
+        <div>
+            <h1>¡Hola Mundo!</h1>
+        </div>
+    );
+}
+
+export default Componente;
